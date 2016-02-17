6968cf02c5d633d1252566ec2624bdbda4c04aa3
christopher <kempt09@gmail.com>
2016-02-17 14:08:18 -0500

    first submission

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-console.log("Hello, World!");
+$(function(){
+	var seatNumber = [];
+	for(var i=1; i<=24; i++){
+		seatNumber.push(i);
+		$(".seat-chart").append(function(){
+			return "<div class='available'><h3>" + i + "</h3></div>";
+		})
+	}
+
+			var seatArray = [];
+	$(".available").on("click", function(){
+
+		var self = this;
+		var seatIndex = $(this).text();
+		var seatBox;
+
+		(function() {
+
+			(function(){
+
+			$(self).toggleClass('reserved');
+
+			})();
+		
+			var x = seatArray.indexOf(seatIndex);
+			if ( x === -1) {
+				seatArray.push(seatIndex);
+				
+			}
+			else {
+				seatArray.splice(x, 1);
+				
+			}
+
+			seatBox = seatArray.join(", ");
+
+			$('#selectedSeat').text(seatBox);
+
+		})();
+
+	});
+
+})
+
+
